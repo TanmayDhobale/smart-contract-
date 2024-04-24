ace2c698eab315de260cecc59d7ad7c669fbde7d
TANMAY <tanmay@TANMAYs-MacBook-Air.local>
2024-04-24 20:40:46 +0530

    created web3 app

---
+++ smartcontract/pages/index.tsx
@@ -0,0 +1,29 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>
+            Welcome to{" "}
+            <span className={styles.gradientText0}> Tanmay`s smart contracts  </span>
+          </h1>
+
+       
+
+          <div className={styles.connect}>
+            <ConnectWallet />
+          </div>
+        </div>
+
+    
+      </div>
+    </main>
+  );
+};
+
+export default Home;
